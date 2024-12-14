39db0eb69ecf1e5931a2d91a18763cdb660f8954
saurabh <saurabh96mishra@gmail.com>
2024-12-14 17:48:49 +0530

    Add Docker Support with multiple images

---
+++ OnlineBanking/src/main/resources/insert_data.sql
@@ -0,0 +1,8 @@
+INSERT into public.admin values (1,'saurabh', '12345678', 'Saurabh@123', '1', 'saurabh');
+INSERT into public.admin values (2,'abhijeet', '12345678', 'Abhijeet@123', '2', 'abhijeet');
+
+INSERT into public.role values (1,'CAPTURER');
+INSERT into public.role values (2,'AUTHORISER');
+
+INSERT into public.adminrole values (1,1);
+INSERT into public.adminrole values (2,2);
